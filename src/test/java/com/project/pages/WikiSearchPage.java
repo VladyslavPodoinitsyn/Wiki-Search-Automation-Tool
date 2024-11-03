310b3d63b4f3787fe682dfd36695ed62dab4d571
Vladyslav Podoinitsyn <133632477+VladyslavPodoinitsyn@users.noreply.github.com>
2024-11-03 15:25:38 -0800

    Create WikiSearchPage.java

---
+++ src/test/java/com/project/pages/WikiSearchPage.java
@@ -0,0 +1,23 @@
+import com.cybertek.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.devtools.v85.page.Page;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class WikiSearchPage {
+
+    public WikiSearchPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(id = "searchInput")
+    public WebElement searchBox;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    public WebElement searchButton;
+
+    @FindBy(id= "firstHeading")
+    public WebElement mainHeader;
+
+
+}
